c24797a9a2289eda49ebc96330aa733ff6de4bba
Navin Mathew <navinmathew3@gmail.com>
2020-02-20 23:37:18 -0600

    first commit, class based components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  //lifecycle method
+  //this runs when the component is loaded.
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

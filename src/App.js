8178c25c2bef8e08b189e9d4bd2cbead9ecdacd0
Navin Mathew <navinmathew3@gmail.com>
2020-02-20 23:42:35 -0600

    function based components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-  //lifecycle method
-  //this runs when the component is loaded.
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello from React</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Hello from react</h1>
+    </div>
+  );
 }
 
 export default App;
